a2ed5ca78a6eff9c5bfb00564734d4b4c11b2a68
Analise Burtet <analiseburtet@gmail.com>
2020-04-02 11:41:24 -0300

    criar comidinha

--- script.js
+++ script.js
@@ -7,6 +7,10 @@ snake[0] = {
     y: 8 * box
 }
 let direction = "right"
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 +1) * box
+}
 
 const criarBG = () => {
     context.fillStyle = "lightgreen"
@@ -20,12 +24,30 @@ const criarCobrinha = () => {
     }
 }
 
+const drawFood = () => {
+    context.fillStyle = "red"
+    context.fillRect(food.x , food.y, box, box)
+}
+
+const update = (event) => {
+    if(event.keyCode == 37 && direction != "right") direction = "left"
+    if(event.keyCode == 38 && direction != "down") direction = "up"
+    if(event.keyCode == 39 && direction != "left") direction = "right"
+    if(event.keyCode == 40 && direction != "up") direction = "down"
+}
+
 const iniciarJogo = () => {
+    if(snake[0].x > 15 * box && direction == "right") snake[0].x = 0
+    if(snake[0].x < 0 && direction == "left") snake[0].x = 16 * box
+    if(snake[0].y > 15 * box && direction == "down") snake[0].y = 0
+    if(snake[0].y < 0 && direction == "up") snake[0].y = 16 * box
+
     criarBG()
     criarCobrinha()
+    drawFood()
 
     let snakeX = snake[0].x
-    let SnakeY = snake[0].y
+    let snakeY = snake[0].y
 
     if(direction == "right") snakeX += box
     if(direction == "left") snakeX -= box
@@ -35,7 +57,7 @@ const iniciarJogo = () => {
     snake.pop()
 
     let newHead = {
-        x: snakeX ,
+        x: snakeX,
         y: snakeY
     }
 
@@ -44,3 +66,5 @@ const iniciarJogo = () => {
 }
 
 let jogo = setInterval(iniciarJogo, 100)
+
+document.addEventListener("keydown", update)
